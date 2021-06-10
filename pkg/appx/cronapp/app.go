886fdbcecaa61b5f5e0b9ab2b31e8a8391b16c18
RussellLuo <luopeng.he@gmail.com>
2021-06-10 11:44:09 +0800

    Improve cronapp

--- pkg/appx/cronapp/app.go
+++ pkg/appx/cronapp/app.go
@@ -2,10 +2,13 @@ package cronapp
 
 import (
 	"github.com/RussellLuo/appx"
+	"github.com/RussellLuo/kok/pkg/appx/wrapper"
 )
 
 type App struct {
 	*appx.App
+
+	wrapper *wrapper.InitWrapper
 
 	scheduler  string
 	expression string
@@ -15,9 +18,23 @@ func New(name string) *App {
 	return &App{App: appx.New(name)}
 }
 
-func (a *App) ScheduledBy(scheduler string) *App {
+func NewV2(name string, instance appx.Initializer) *App {
+	w := wrapper.New(instance)
+	return &App{
+		App:     appx.NewV2(name, w),
+		wrapper: w,
+	}
+}
+
+func (a *App) ScheduledBy(scheduler string, expression ...string) *App {
 	a.scheduler = scheduler
 	a.App.Require(scheduler)
+
+	if len(expression) == 1 {
+		a.expression = expression[0]
+		a.wrapper.AfterInitFunc(a.afterInit)
+	}
+
 	return a
 }
 
@@ -60,4 +77,32 @@ func (a *App) scheduledBy(initFunc appx.InitFuncV2) appx.InitFuncV2 {
 		scheduler.Add(a.Name, a.expression, job.Task) // nolint:errcheck
 		return nil
 	}
+}
+
+func (a *App) afterInit(ctx appx.Context) error {
+	var scheduler Scheduler
+	var err error
+
+	instance := ctx.MustLoad(a.scheduler)
+	if inst, ok := instance.(CronScheduler); ok {
+		scheduler, err = GetCronScheduler(inst)
+		if err != nil {
+			return err
+		}
+	} else {
+		// The parent app is an old-style one.
+		// TODO: remove this snippet when all apps are new-style ones.
+		scheduler, err = GetScheduler(ctx.Required[a.scheduler].Value)
+		if err != nil {
+			return err
+		}
+	}
+
+	job, err := GetCronJob(a.wrapper.Instance())
+	if err != nil {
+		return err
+	}
+
+	scheduler.Add(a.Name, a.expression, job.Task) // nolint:errcheck
+	return nil
 }
--- pkg/appx/cronapp/value.go
+++ pkg/appx/cronapp/value.go
@@ -29,3 +29,41 @@ func GetJob(value interface{}) (Job, error) {
 	}
 	return j, nil
 }
+
+// CronScheduler is the interface that a scheduler application must implement.
+type CronScheduler interface {
+	Scheduler() Scheduler
+}
+
+// CronJob is the interface that a job application must implement.
+type CronJob interface {
+	Job() Job
+}
+
+func GetCronScheduler(value interface{}) (Scheduler, error) {
+	r, ok := value.(CronScheduler)
+	if !ok {
+		return nil, fmt.Errorf("value %#v does not implement httpapp.CronScheduler", value)
+	}
+
+	result := r.Scheduler()
+	if result == nil {
+		return nil, fmt.Errorf("method Scheduler() of value %#v returns nil", value)
+	}
+
+	return result, nil
+}
+
+func GetCronJob(value interface{}) (Job, error) {
+	r, ok := value.(CronJob)
+	if !ok {
+		return nil, fmt.Errorf("value %#v does not implement httpapp.CronJob", value)
+	}
+
+	result := r.Job()
+	if result == nil {
+		return nil, fmt.Errorf("method Job() of value %#v returns nil", value)
+	}
+
+	return result, nil
+}
