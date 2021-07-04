050d2a1f4c537284031c552c3ecc21eafb1ebce2
RussellLuo <luopeng.he@gmail.com>
2021-07-04 23:21:56 +0800

    Update grpccodec

--- pkg/grpccodec/codec.go
+++ pkg/grpccodec/codec.go
@@ -7,11 +7,11 @@ import (
 // Codec is a series of codecs (encoders and decoders) for gRPC requests and responses.
 type Codec interface {
 	// DecodeRequest converts a proto message to a Go value.
-	// It is used at the server side.
+	// It is designed to be used at the server side.
 	DecodeRequest(pb proto.Message, out interface{}) error
 
 	// EncodeResponse converts a Go value to a proto message.
-	// It is used at the server side.
+	// It is designed to be used at the server side.
 	EncodeResponse(in interface{}, pb proto.Message) error
 }
 
