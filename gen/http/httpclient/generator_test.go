82a09a2cfc74bf35397e31ebe1a0798123189aca
nicolaa5 <niekbijman@gmail.com>
2022-08-26 09:02:26 +0200

    add testcases

---
+++ gen/http/httpclient/generator_test.go
@@ -0,0 +1,69 @@
+package httpclient
+
+import (
+	"go/types"
+	"testing"
+
+	"github.com/RussellLuo/kun/pkg/ifacetool"
+)
+
+func TestReturnError(t *testing.T) {
+	for _, tt := range []struct {
+		name  string
+		input *ifacetool.Param
+		want  string
+	}{
+		{
+			name: "int",
+			input: &ifacetool.Param{
+				Name:       "param",
+				TypeString: "int",
+				Type:       types.Typ[types.Int],
+			},
+			want: "0",
+		},
+		{
+			name: "string",
+			input: &ifacetool.Param{
+				Name:       "param",
+				TypeString: "string",
+				Type:       types.Typ[types.String],
+			},
+			want: `""`,
+		},
+		{
+			name: "bool",
+			input: &ifacetool.Param{
+				Name:       "param",
+				TypeString: "bool",
+				Type:       types.Typ[types.Bool],
+			},
+			want: "false",
+		},
+		{
+			name: "struct",
+			input: &ifacetool.Param{
+				Name:       "param",
+				TypeString: "MyStruct",
+				Type:       &types.Struct{},
+			},
+			want: "MyStruct{}",
+		},
+		{
+			name: "iface",
+			input: &ifacetool.Param{
+				Name:       "param",
+				TypeString: "MyInterface",
+				Type:       &types.Interface{},
+			},
+			want: "nil",
+		},
+	} {
+		t.Run("test return error", func(t *testing.T) {
+			got := createReturnError(tt.input)
+			if got != tt.want {
+				t.Fatalf("got (%v), want (%#v)", got, tt.want)
+			}
+		})
+	}
+}
